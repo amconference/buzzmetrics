5c5dedf34d3bba17ffcdd6aa3056889dc94d62f8
Matt Peperell <mattp@altmetric.com>
2014-09-24 14:42:42 +0100

    add word_prevalence to the rake task

--- ruby/buzzmetrics/lib/buzzmetrics.rb
+++ ruby/buzzmetrics/lib/buzzmetrics.rb
@@ -3,6 +3,7 @@ require "buzzmetrics/version"
 require 'buzzmetrics/question_mark'
 require 'buzzmetrics/colons'
 require 'buzzmetrics/analyse_title_length'
+require 'buzzmetrics/word_prevalence'
 
 module Buzzmetrics
   # Your code goes here...
